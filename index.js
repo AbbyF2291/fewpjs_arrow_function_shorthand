4c6368bdd08452cfa31212ff6fcc57916e0f99aa
AbbyFreund <abby_freund@hotmail.com>
2021-04-12 19:37:00 +1000

    Completed

---
+++ index.js
@@ -0,0 +1,11 @@
+let divide = (parameter1, parameter2) => 2000 / 100;
+
+console.log (divide(2000, 100))
+
+let square = x => x * x;
+
+console.log (square(2));
+
+let add = (a, b) => a + b;
+
+console.log (add(3, 4))
